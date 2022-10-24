28ea0e0272bef1397ffe15c6a26932e482227996
Ingrid O'Connor <ingridoco@gmail.com>
2022-10-24 16:28:27 -0500

    set up file / added an example call to wiki API

---
+++ WikiHelper.py
@@ -0,0 +1,33 @@
+# Example call to GET lists with WikiMedia API
+
+
+"""
+    get_links.py
+
+    MediaWiki API Demos
+    Demo of `Links` module: Get all links on the given page(s)
+
+    MIT License
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    "action": "query",
+    "format": "json",
+    "titles": "Albert Einstein",
+    "prop": "links"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+PAGES = DATA["query"]["pages"]
+
+for k, v in PAGES.items():
+    for l in v["links"]:
+        print(l["title"])
