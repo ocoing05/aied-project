1a2a255c364498b656c37132f0c70c7f375e84b2
Ingrid O'Connor <ingridoco@gmail.com>
2022-10-25 17:52:32 -0500

    set up getLinkedPages function

--- WikiHelper.py
+++ WikiHelper.py
@@ -1,33 +1,50 @@
-# Example call to GET lists with WikiMedia API
-
-
-"""
-    get_links.py
-
-    MediaWiki API Demos
-    Demo of `Links` module: Get all links on the given page(s)
-
-    MIT License
-"""
-
 import requests
 
-S = requests.Session()
+class WikiHelper:
+    def __init__(self):
+        # Session object allows one to persist certain parameters across requests.
+        self.S = requests.Session()
+        self.URL = "https://en.wikipedia.org/w/api.php"
 
-URL = "https://en.wikipedia.org/w/api.php"
+    # Returns list of titles of all links from the given page
+    def getLinkedPages(self, title):
+        params = {
+            "action": "query",
+            "format": "json",
+            "titles": title,
+            "prop": "links",
+            "pllimit": 500
+        }
 
-PARAMS = {
-    "action": "query",
-    "format": "json",
-    "titles": "Albert Einstein",
-    "prop": "links"
-}
+        linkedPages = []
+        morePages = True
+        while morePages: 
+            res = self.S.get(url=self.URL, params=params)
+            resJSON = res.json()
 
-R = S.get(url=URL, params=PARAMS)
-DATA = R.json()
+            pages = resJSON["query"]["pages"]
 
-PAGES = DATA["query"]["pages"]
+            for i, j in pages.items(): 
+            # i = number representing the original page (key), j = the actual content containing everything (value)
+                for k in j["links"]: # "links" is an attribute inside of j and contains the actual list of pages
+                    if k["ns"] == 0: # ns = 0 are actual pages
+                        linkedPages.append(k["title"])
 
-for k, v in PAGES.items():
-    for l in v["links"]:
-        print(l["title"])
+            # if there are still more pages to load, we will call again from where we left off
+            if (not "continue" in resJSON):
+                morePages = False
+            else:
+                cont = resJSON["continue"]["plcontinue"] # used to get more pages
+                params["plcontinue"] = cont
+
+        return linkedPages
+
+# example call
+wh = WikiHelper()
+pages = wh.getLinkedPages("Aristotle")
+print(pages)
+
+# NOTES : 
+# i feel like there is a way to be neater about the code above ? but for now it works
+# there are a LOT of links for each page and still not entirely sure if they're all right... they all seem related but can't find them all on actual pages
+# maybe we need to filter down somehow from there..?
