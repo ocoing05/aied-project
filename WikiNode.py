573c338c340c235b3aef596d3bde83dbf1453120
Ingrid O'Connor <ingridoco@gmail.com>
2022-10-27 19:51:18 -0500

    Update WikiNode.py

--- WikiNode.py
+++ WikiNode.py
@@ -7,17 +7,32 @@ Uses NLP on content to determine key words, which are used by recommender system
 import wikipedia
 
 class WikiNode:
-    def __init__(self, title):
+    def __init__(self, title, prevNode):
         self.title = title
-        self.url = "https://en.wikipedia.org/wiki/" + title
-        
-        page = wikipedia.page(title)
-        self.summary = wikipedia.summary(title, sentences = 3)
-        self.categories = page.categories
-        self.linkedPages = page.links
-        self.content = page.content
 
-        # TODO: use NLP to return list of most important words/topics ???
-        self.keyWords = []
+        self.page = wikipedia.page(self.title)
+        # these would be like children in the tree ?
+        self.linkedPages = self.page.links
 
-    # do we need getter functions or can we access the variables directly ??
+        # parent in the tree ? ... the article they read to get this suggestion
+        # not sure if this is needed
+        self.prevNode = prevNode
+
+        self.keyWords = self.getKeyWords()
+
+    def getURL(self):
+        return "https://en.wikipedia.org/wiki/" + self.title
+
+    def getCategories(self):
+        return wikipedia.page(self.title).categories
+
+    def getContent(self):
+        return wikipedia.page(self.title).content
+
+    def getSummary(self, sentences):
+        self.summary = wikipedia.summary(self.title, sentences)
+
+    def getKeyWords(self):
+        text = self.getContent()
+        # use NLP to get key words for the article which can be used by recommender
+        pass
