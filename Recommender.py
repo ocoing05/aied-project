1b21b692d0ce53f3a1e4f3d387aa45093335fe19
quentinrh12 <quentinrh12@gmail.com>
2022-10-31 11:50:52 -0500

    Update Recommender.py

--- Recommender.py
+++ Recommender.py
@@ -1,9 +1,19 @@
 import StudentModel
+import WikiNode
 
 class Recommender:
     
     def __init__(self, studentModel) -> None:
         
         self.studentModel = studentModel
+        self.recommendations = {} # dictionary of WikiNodes and a value between 0 and 1, indicating quality 
 
+    def getRecommendations(self) -> list:
         
+        return self.recommendations.keys()
+
+    def generateNewRecommendations(self) -> None:
+
+        # self.studentModel.getFringe
+
+        pass
