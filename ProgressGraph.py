0328049e7a27f2fac35286131e499c058f98c45b
quentinrh12 <quentinrh12@gmail.com>
2022-11-01 11:48:37 -0500

    Update ProgressGraph.py

--- ProgressGraph.py
+++ ProgressGraph.py
@@ -3,18 +3,18 @@ class ProgressGraph:
 
     def __init__(self) -> None:
 
-        self.opened = {} # a dictionary of all the WikiNodes opened at least once by the Student
+        self.visited = {} # a dictionary of all the WikiNodes opened at least once by the Student
 
     def update(self, wikiNode) -> None:
 
-        # self.opened[wikiNode] = [listOfSpecialKeywords]
+        # self.visited[wikiNode] = [listOfSpecialKeywords]
         # if self.fringe contains wikiNode, remove it
         # add wikiNode.fringeSet to self.fringe
         pass
 
     def getExplored(self) -> set:
 
-        return self.opened.keys()
+        return self.visited.keys()
 
     def updateFringePriority(self) -> None:
 
