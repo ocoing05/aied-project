7d8321623709b80ca4066fbf2d25179e04e94339
quentinrh12 <quentinrh12@gmail.com>
2022-11-01 09:05:41 -0500

    Update StudentModel.py

--- StudentModel.py
+++ StudentModel.py
@@ -14,6 +14,7 @@ class StudentModel:
             self.interestKeywords[interest] = 1
 
         self.progressGraph = ProgressGraph()
+        self.fringeQueue = {} # Priority Queue of unopened WikiNodes adjacent to progressGraph, sorted by potential interest
         self.newWords = [] # List of words identified by the student as unknown/that they are unsure of the meaning
 
     def getStudentName(self) -> str:
@@ -24,6 +25,18 @@ class StudentModel:
 
         return self.interestKeywords.keys()
 
+    def updateFringe(self) -> None:
+        
+        for key in self.fringeQueue:
+            words = key.getKeywords()
+            interestCounter = 0
+            for word in words:
+                # if (word is in self.studentModel.interestKeywords):
+                    # interestCounter += self.studentModel.interestKeywords[word]
+                pass
+
+            potentialInterest = interestCounter / len(words)
+            self.fringeQueue[key] = potentialInterest
 
 
 
