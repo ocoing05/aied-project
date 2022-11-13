e2d7c47b991107842ba4145eb7a6fc2a65792f43
Ingrid O'Connor <ingridoco@gmail.com>
2022-11-13 15:31:27 -0600

    started creating MVP UI in terminal

---
+++ FreeLearner.py
@@ -0,0 +1,22 @@
+'''
+Runs MVP of FreeLearner
+'''
+
+from StudentModel import StudentModel
+
+
+def runFreeLearnerMVP():
+    print('Welcome to FreeLearner!')
+    name = input('What is your name? ')
+    print('Hi, ', name, '!')
+    print('Please write a list of things that you would like to learn about.')
+    print('This list should be separated by commas, like this: Dinosaurs,Science,Disney')
+    interestStr = input('What are your interests? \n')
+    student = StudentModel(name, '', '', '', createInterestList(interestStr))
+
+# takes in a string of interests separated by commas and creates an interest list
+def createInterestList(str):
+    pass
+
+if __name__ == "__main__":
+    runFreeLearnerMVP()
