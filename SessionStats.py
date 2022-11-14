add386f98372e909f45eef7bdf53afaedb5cacdd
quentinrh12 <quentinrh12@gmail.com>
2022-11-14 12:41:02 -0600

    Building classes that handle and record individual session data

--- SessionStats.py
+++ SessionStats.py
@@ -1,3 +1,27 @@
+
 class SessionStats:
+    
     def __init__(self) -> None:
-        pass
+
+        self.path = [] # list of wikiNodes, where the index is the order visited 
+        self.nodeStats = {} # dictionary of wikiNodes visited this session, and a list of the stats accumulated
+        self.statNames = ["elapsedTime", "erosionTime", "numVisits", "numSectionTests"]
+
+    def addToPath(self, wikiNode, statDict) -> None:
+        """This function is called when the student moves from one node to another,
+        and """
+        self.path.append(wikiNode)
+
+        if not self.nodeStats[wikiNode]:
+            self.nodeStats[wikiNode] = statDict
+        else:
+            prevStats = self.nodeStats[wikiNode]
+            newStats = {}
+            for stat in prevStats.keys():
+                prevVal = prevStats[stat] 
+                newVal = statDict[stat]
+                newStats[stat] = newVal + prevVal
+
+    def getNodeStats(self, wikiNode) -> dict:
+        return self.nodeStats[wikiNode]
+                
--- SessionTracker.py
+++ SessionTracker.py
@@ -1,5 +1,17 @@
 import SessionStats
+import time 
 
 class SessionTracker:
+
     def __init__(self) -> None:
+
+        self.sessionStats = SessionStats()
+        self.startTime = 0
+        self.currNode = None
         pass
+
+    def startTiming(self, wikiNode) -> None:
+        
+        self.currNode = wikiNode
+        self.startTime = time.time()
+
