ee6138a1cb44dde6ae6172bb8b3081a6b699c71d
quentinrh12 <quentinrh12@gmail.com>
2022-11-18 17:33:24 -0600

    Update domainmodel.py

--- domainmodel.py
+++ domainmodel.py
@@ -5,8 +5,7 @@ import networkx as nx
 from FoxQueue import PriorityQueue
 from wikinode import WikiNode
 from mediawiki import MediaWiki
-
-# from owlready2 import *
+from owlready2 import *
 
 class DomainModel:
     
@@ -17,8 +16,8 @@ if __name__ == "__main__":
 
     wikipedia = MediaWiki() 
 
-    # onto = get_ontology("file:///Users/quentinharrington/Desktop/COMP484/aied-project/wiki_cats_full_non_cyclic_v1.owl")
-    # onto.load()
-    # ontoList = list(onto.classes())
-    # print(ontoList)
+    onto = get_ontology("file:///Users/quentinharrington/Desktop/COMP484/aied-project/wiki_cats_full_non_cyclic_v1.owl")
+    onto.load()
+    ontoList = list(onto.classes())
+    print(ontoList)
 
