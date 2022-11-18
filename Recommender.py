0c2a70acba43d2134f8a9aad0e01f975cfab231f
quentinrh12 <quentinrh12@gmail.com>
2022-11-18 10:30:39 -0600

    Update Recommender.py
    Added spacy + sense2vec
    changed to AdaptiveModel

--- Recommender.py
+++ Recommender.py
@@ -1,12 +1,14 @@
 import StudentModel
 import DomainModeling
+import spacy
+from sense2vec import Sense2Vec
 
-class Recommender:
+class AdaptiveModel:
     
     def __init__(self) -> None:
         
         self.studentModel = None
-        self.recommendations = {} # dictionary of WikiNodes and a value between 0 and 1, indicating quality, ordered by quality 
+        self.recommendations = {} # dictionary of WikiNodes and a value between 0 and 1, indicating quality
 
     def setStudentModel(self, studentModel) -> None:
         
@@ -41,3 +43,14 @@ class Recommender:
 # compare student progress graph with ontology graph to see which are explored
 # maybe just get the top n in the student's queue to go into Recommender
 # take articles from student queue and sort those into categories based on domains in ontology model
+
+if __name__ == "__main__":
+    pass
+    # nlp = spacy.load('en_core_web_lg')
+    # s2v = nlp.add_pipe("sense2vec")
+    # s2v.from_disk("/path/to/s2v_reddit_2015_md")
+
+    # doc1 = s2v("Alien")
+    # doc2 = s2v("Extraterrestrial")
+
+    # print(doc1.similarity(doc2))
